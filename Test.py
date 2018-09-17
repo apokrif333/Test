17eefc07a4447849b617c0d7c93721291a844f3e
Alex.Ash <alex.ah@list.ru>
2018-09-17 11:27:49 -0400

    17.09.2018 Work

--- Test.py
+++ Test.py
@@ -17,6 +17,15 @@ def create_file():
     print(f'Time for create file by  is {t2 - t1} seconds')
 
 
+def little_file():
+    np_array = np.random.rand(4, 5, 5)
+    np.save('Temp.npy', np_array)
+
+
+def filter_by_column(file: np.ndarray, column: int, value: float) -> np.array:
+    pp(file)
+    return file[file[:, :, column] > value][:, np.array([False, False, True, False, True])]
+
 # create_file()
 # with open('Temp.txt', 'r') as f:
 #     f_data = f.read()
@@ -27,10 +36,5 @@ def create_file():
 # np.save('Temp.npy', np_array)
 
 
-main_dict = {'One': [1, 2, 3, 4], 'Two': ['wer', 'rsdf', 'dax']}
-names = ['id', 'data']
-formats = ['f8', 'f8']
-dtype = dict(names=names, formats=formats)
-array = np.array(list(main_dict.items()), dtype=dtype, count=len(main_dict))
-print(array)
+pp(filter_by_column(np.load('Temp.npy'), 0, 0.5))
 
