14ca91677be2bc9dc575ec141ec5c9199283de74
Alex.Ash <alex.ah@list.ru>
2018-10-02 13:17:30 -0400

    02.10.2018 Work

--- Stocks/02_check_tickers.py
+++ Stocks/02_check_tickers.py
@@ -2,14 +2,18 @@ import pandas as pd
 import os
 import sys
 
-sys.path.insert(0, 'C:/Users/Tom/PycharmProjects/Start/GibHub/My_Libs')
+sys.path.insert(0, 'C:/Users/Lex/PycharmProjects/Start/GitHub/My_Libs')
 import trading_lib as tl
 
 file = pd.read_excel('earnings/Portfolio123.xlsx')
 tickers_list = file['Ticker'].unique()
+length = len(tickers_list)
 
 for t in tickers_list:
+    index = tickers_list.tolist().index(t)
+    print(length - index)
     if os.path.isfile('data/yahoo/daily_new/' + str(t) + '.csv'):
-        print(t)
+        print(t + ' normal')
     else:
+        print(t + ' download')
         tl.download_yahoo(t, 'data/yahoo/daily_new/')
